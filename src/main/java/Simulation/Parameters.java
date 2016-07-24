c8f6f290e0a804c5cc467ac830e0611967ffeb5a
Zichuan Xu <zichuanxu.mail@gmail.com>
2016-07-24 07:26:17 +0100

    change bandwidth cost parameters

--- src/main/java/Simulation/Parameters.java
+++ src/main/java/Simulation/Parameters.java
@@ -137,7 +137,7 @@ import Algorithm.CostFunctions.ExponentialCostFunction;
     private double linkDelayReqMax = 5; // maximum link delay
 
     private double linkCostMax = 0.001;
-    private double linkCostMin = 0.1;
+    private double linkCostMin = 0.05;
 
     /**
      * Request related parameters
